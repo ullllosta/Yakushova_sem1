d6b1295bf1a26f98138c21c9d811003b119bb094
lizay <lizayakushova172@gmail.com>
2021-12-19 16:02:30 +0300

    9 практическая(переделанная)

--- PZ_9/PZ9_1.py
+++ PZ_9/PZ9_1.py
@@ -6,21 +6,40 @@
 # 2)В каких магазинах можно приобрести одновременно молоко и сахар.
 # 3)В каких магазинах можно приобрести соль.
 
-shop = {'Магнит': ['молоко', 'соль', 'сахар'], 'Пятерочка': ['мясо', 'молоко', 'сыр'],
-        'Перекресток': ['молоко', 'творог', 'сыр', 'сахар']}
+magnit = {'молоко', 'соль', 'сахар'}
+pyaterochka = {'мясо', 'молоко', 'сыр'}
+perekrestok = {'молоко', 'творог', 'сыр', 'сахар'}
+a = set()
+
+
 # 1
-for x in shop:
-    if 'сыр' not in shop[x]:
-        print('Сыра нет в: ', x)
+for i in magnit, pyaterochka, perekrestok:
+    if not 'сыр' in magnit:
+        a.add('магнит')
+    if not 'сыр' in pyaterochka:
+        a.add('пятерочка')
+    if not 'сыр' in perekrestok:
+        a.add('перекресток')
+print('Сыр нельзя преобрести в магазинах: ', a), a.clear()
 
 
 # 2
-for x in shop:
-    if 'молоко' and 'сахар' in shop[x]:
-        print('Молоко и сахар есть в: ', x)
+for i in magnit, pyaterochka, perekrestok:
+    if 'молоко' and 'сахар' in magnit:
+        a.add('магнит')
+    if 'молоко' and 'сахар' in pyaterochka:
+        a.add('пятерочка')
+    if 'молоко' and 'сахар' in perekrestok:
+        a.add('перекресток')
+print('Молоко и сахар можно преобрести в магазинах: ', a), a.clear()
 
 
 # 3
-for x in shop:
-    if 'соль' in shop[x]:
-        print('Соль есть в: ', x)
+for i in magnit, pyaterochka, perekrestok:
+    if 'соль' in magnit:
+        a.add('магнит')
+    if 'соль' in pyaterochka:
+        a.add('пятерочка')
+    if 'соль' in perekrestok:
+        a.add('перекресток')
+print('соль можно преобрести в магазинах: ', a)
